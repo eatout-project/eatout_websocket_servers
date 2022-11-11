1ad1ab2056790c1a95abe7fb1ba21fee27f81178
chma <christian.em@live.dk>
2022-11-11 01:07:34 +0100

    EAT-34 made serverside for socket connection and kafka

---
+++ reservation_socket/businessobjects/businessobjects.ts
@@ -0,0 +1,8 @@
+export interface ReservationInfoApiObject {
+    restaurantName: string;
+    amountOfGuests: number;
+    timeOfArrival: string;
+    restaurantId: number;
+    customerId: number;
+    customerName: string;
+}
